8fe5f64fb331c8a9455c6c719dea1f6a349489dd
吴伟城 <wuvcen@Wuvcens-MBP.local>
2015-11-01 09:49:36 +0800

    common

---
+++ Reader/PageModelController.h
@@ -0,0 +1,14 @@
+//
+//  PageModelController.h
+//  Reader
+//
+//  Created by 吴伟城 on 15/11/1.
+//  Copyright © 2015年 吴伟城. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PageViewController.h"
+
+@interface PageModelController : NSObject <UIPageViewControllerDataSource>
+
+@end
---
+++ Reader/PageModelController.m
@@ -0,0 +1,13 @@
+//
+//  PageModelController.m
+//  Reader
+//
+//  Created by 吴伟城 on 15/11/1.
+//  Copyright © 2015年 吴伟城. All rights reserved.
+//
+
+#import "PageModelController.h"
+
+@implementation PageModelController
+
+@end
